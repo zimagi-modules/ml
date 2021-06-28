ce230dae94613f9190f0311eda39562c3b5f900d
Adrian Webb <adrian@webb.sh>
2021-06-28 03:17:44 -0400

    Updates to the model command mixin.

--- commands/mixins/model.py
+++ commands/mixins/model.py
@@ -11,15 +11,24 @@ class ModelMixin(CommandMixin('model')):
         return project_dir(settings.MODEL_PROJECT_NAME, settings.MODEL_PROJECT_MODEL_DIR)
 
 
-    def get_model(self, name, **params):
-        if 'X_data' in params and isinstance(params['X_data'], str):
-            params['X_data'] = self.load_data(params['X_data'])
+    def get_model(self, name, model_params, data_params):
+        model_params['data_provider'] = self.get_data_provider()
 
-        if 'Y_data' in params and isinstance(params['Y_data'], str):
-            params['Y_data'] = self.load_data(params['Y_data'])
+        if 'X_data' in model_params and isinstance(model_params['X_data'], str):
+            index_column = model_params.pop('X_data_index_column') if 'X_data_index_column' in model_params else 'date'
+            model_params['X_data'] = self.load_data(model_params['X_data'], index_column = index_column)
 
-        return self.get_model_class()(name, **params)
+        if 'Y_data' in model_params and isinstance(model_params['Y_data'], str):
+            index_column = model_params.pop('Y_data_index_column') if 'Y_data_index_column' in model_params else 'date'
+            model_params['Y_data'] = self.load_data(model_params['Y_data'], index_column = index_column)
+
+        return self.get_provider('ml_model', self.get_model_provider(),
+            name, model_params, data_params
+        )
 
 
-    def get_model_class(self):
-        raise NotImplementedError("Implement get_model_class in derived classes of the Model Command Mixin")
+    def get_model_provider(self):
+        raise NotImplementedError("Implement get_model_provider in derived classes of the Model Command Mixin")
+
+    def get_data_provider(self):
+        raise NotImplementedError("Implement get_data_provider in derived classes of the Model Command Mixin")
