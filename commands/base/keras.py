ceb071aeaff1af6da5993dac251f146ac3a0d8e9
Adrian Webb <adrian@webb.sh>
2021-06-28 03:18:37 -0400

    Adding initial base commands and related specifications.

---
+++ commands/base/keras.py
@@ -0,0 +1,27 @@
+from systems.commands.index import BaseCommand
+
+
+class ModelBaseCommand(BaseCommand('keras')):
+
+    def get_model_provider(self):
+        return 'keras'
+
+    def get_data_provider(self):
+        return 'keras'
+
+    def data_parameters(self):
+        return { **super().data_parameters(),
+            'training_index': self.training_index,
+            'validation_index': self.validation_index,
+            'test_index': self.test_index
+        }
+
+
+    def train(self, model, params):
+        self.notice('Training model')
+        return model.train(
+            epochs = self.epochs,
+            batch_size = self.batch_size,
+            verbose = not settings.API_EXEC,
+            **params
+        )
---
+++ commands/base/model.py
@@ -0,0 +1,72 @@
+from django.conf import settings
+
+from systems.commands.index import BaseCommand
+
+
+class ModelBaseCommand(BaseCommand('model')):
+
+    def model_parameters(self):
+        return {}
+
+    def data_parameters(self):
+        return {
+            'split_percentages': self.split_percentages
+        }
+
+    def train_parameters(self):
+        return {}
+
+    def predict_parameters(self):
+        return {}
+
+
+    def exec(self):
+        self.run_model()
+
+    def run_model(self, **params):
+        model = self.init_model(
+            self.model_parameters(),
+            self.data_parameters()
+        )
+        training = self.train(model, self.train_parameters())
+        test_data = self.predict(model, self.predict_parameters())
+        return model, training, test_data
+
+
+    def init_model(self, model_params, data_params):
+        model_params['X_data'] = self.predictor_data_name
+        model_params['Y_data'] = self.target_data_name
+
+        model = self.get_model(self.model_name,
+            model_params, data_params
+        )
+        self.notice('Building model')
+        model.build(self.build_model)
+
+        self.data('Model', self.model_name)
+        self.info(model.summary())
+        return model
+
+    def build_model(self, data):
+        # Override in subclass
+        raise NotImplementedError("Subclasses of the base model command must implement a build_model method")
+
+
+    def train(self, model, params):
+        self.notice('Training model')
+        return model.train(**params)
+
+    def predict(self, model, params):
+        self.notice('Running test predictions and generating results')
+        test_data = model.predict(**params)
+
+        if self.plot_columns:
+            indexes = self.plot_prediction_indexes if self.plot_prediction_indexes else None
+
+            self.notice('Generating test plots')
+            for column in self.plot_columns:
+                model.plot(column, test_data,
+                    *model.get_prediction_columns(column, indexes = indexes)
+                )
+        self.success('Successfully tested and generated model results')
+        return test_data
