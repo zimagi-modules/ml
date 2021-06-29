2406aee54764fa7898bdd5bc07e7844e4d8b463d
Adrian Webb <adrian@webb.sh>
2021-06-29 07:51:58 -0400

    Adding single target selection in the keras sequence ml data provider and related specification.

--- plugins/ml_data/keras_sequence.py
+++ plugins/ml_data/keras_sequence.py
@@ -9,7 +9,11 @@ class Provider(BaseProvider("ml_data", "keras_sequence")):
         self.training_frame = self.reframe(self.training_data)
         self.validation_frame = self.reframe(self.validation_data)
         self.test_frame = self.reframe(self.test_data)
-        self.period = self.field_Y_period if self.field_target else self.field_X_period
+
+        if self.field_target:
+            self.period = 1 if self.field_single_event else self.field_Y_period
+        else:
+            self.period = self.field_X_period
 
 
     def reframe(self, series):
@@ -25,7 +29,10 @@ class Provider(BaseProvider("ml_data", "keras_sequence")):
                 break
 
             if self.field_target:
-                results.append(series[X_end:Y_end,:])
+                if self.field_single_event:
+                    results.append(series[(Y_end - 1):Y_end,:])
+                else:
+                    results.append(series[X_end:Y_end,:])
             else:
                 results.append(series[window_start:X_end,:])
 
